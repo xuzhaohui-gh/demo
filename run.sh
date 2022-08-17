31a22e9977ca64113c75a244966861fde765b89e
xuzhaohui-gh <38806974+xuzhaohui-gh@users.noreply.github.com>
2022-08-17 17:43:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 
 # 进入测试用例源码目录
-cd workspace
+# cd workspacei
 
 # 删除上次生成的报告文件
-rm -rf cypress/result
+rm -rf cypress/results
 rm -rf cypress/videos
 
 # 创建容器并执行测试用例
@@ -15,7 +15,7 @@ npx mochawesome-report-generator merge-report.json
 
 # 拷贝测试报告至jenkins流水线任务工作空间目录供查看报告
 # cd ../../
-# scp -r /root/workspace/mochawesome-report/ /var/lib/jenkins/workspace/自动化测试/
+scp -r /root/workspace/mochawesome-report/ /var/lib/jenkins/workspace/自动化测试/
 
 # 必须，否则jenkins任务无法正常退出，会认为任务失败
 exit 0;
