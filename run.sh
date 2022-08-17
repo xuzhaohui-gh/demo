7474a377859c71ca3e06531ef15248889a9ea262
xuzhaohui-gh <38806974+xuzhaohui-gh@users.noreply.github.com>
2022-08-17 16:05:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
+# 进入测试用例源码目录
+cd workspace
+
+# 创建容器并执行测试用例
 docker run -it -v $PWD:/test -w /test cypress3.8.3:latest;
+
+# 将测试报告合成一个html入口
+npx mochawesome-merge cypress/results/*.json > merge-report.json
+npx mochawesome-report-generator merge-report.json
+
+# 拷贝测试报告至jenkins流水线任务工作空间目录供查看报告
+cd ../../
+scp -r /root/workspace/mochawesome-report/ /var/lib/jenkins/workspace/自动化测试/
+
+# 必须，否则jenkins任务无法正常退出，会认为任务失败
 exit 0;
