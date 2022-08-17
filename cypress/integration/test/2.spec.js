0dbf4dafba8aacf5a704cfdce1a2fd5c61465819
xuzhaohui-gh <38806974+xuzhaohui-gh@users.noreply.github.com>
2022-08-17 17:49:32 +0800

    Update 2.spec.js

--- cypress/integration/test/2.spec.js
+++ cypress/integration/test/2.spec.js
@@ -1,5 +1,5 @@
-describe('我的第一个测试', function() {
-  it('百度测试用例：', function() {
+describe('我的第二个测试', function() {
+  it('失败的测试用例：', function() {
     cy.visit('https://e.vip.com')
     cy.get('.login-btn1234').first().click()
   })
